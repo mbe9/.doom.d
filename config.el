7d4cb4d1ca150945baf725ddd167b416fe960d40
Pavel Pletnev <pletnev.pg@gmail.com>
2020-06-21 15:07:04 +0300

    Clean up config a bit

--- config.el
+++ config.el
@@ -1,32 +1,48 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
+(defmacro is-device-laptop () '(string= (system-name) "Archie"))
+(defmacro is-device-main () '(string= (system-name) "Pavels-Mac-mini.local"))
+
+;; General settings
 (setq user-full-name "Pavel Pletnev"
       user-mail-address "pletnev.pg@gmail.com"
 
       doom-theme 'doom-one
 
       display-line-numbers-type 'relative
-
-      ;; Increase delay to reduce fp popups
-      which-key-idle-delay 2.0
-
-      ;; I can wait, if it means less server crashes
-      lsp-idle-delay 2.5
-      lsp-ui-sideline-enable nil
-      lsp-enable-symbol-highlighting nil
-
-      ;; Check files only on save and mode enable
-      flycheck-check-syntax-automatically '(save mode-enabled)
-      ;; Disable max error limit
-      flycheck-checker-error-threshold nil
-
-      ;; Use the same font size for Zen mode
-      +zen-text-scale 0
-      ;; Increase default line width for Zen mode
-      writeroom-width 120
-
-      magit-repository-directories '(("~/projects" . 2))
       )
+
+;; In an unlikely case I wish to use GUI
+(cond ((is-device-laptop)
+       (setq doom-font (font-spec :family "monospace" :size 12)))
+      (t
+       (setq doom-font (font-spec :family "monospace" :size 14))))
+
+(after! magit
+  (setq magit-repository-directories '(("~/projects" . 2))))
+
+(after! writeroom-mode
+  (setq
+   ;; Use the same font size for Zen mode
+   +zen-text-scale 0
+   ;; Increase default line width for Zen mode
+   writeroom-width 120))
+
+(after! flycheck-mode
+  (setq flycheck-checker-error-threshold 1000
+        ;; Check files only on save and mode enable
+        flycheck-check-syntax-automatically '(save mode-enabled)))
+
+(after! lsp-mode
+  (setq lsp-idle-delay 2.5
+        lsp-enable-symbol-highlighting nil))
+
+(after! lsp-ui
+  (setq lsp-ui-sideline-enable nil))
+
+;; Increase delay to reduce fp popups
+(after! which-key
+  (setq which-key-idle-delay 2.0))
 
 ;; Do not hide non-active #ifdefs
 (after! emacs-ccls
@@ -38,11 +54,6 @@
 
 ;; Try to get rid of screen flickering
 (add-to-list 'default-frame-alist '(inhibit-double-buffering . t))
-
-;; Prefer opening popup windows to the side, not at the bottom
-;; (plist-put +popup-defaults :side 'right)
-;; (plist-put +popup-defaults :width 0.5)
-
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -66,9 +77,3 @@
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
 
-(cond
- ((eq (system-name) "Archie")
-  (setq doom-font (font-spec :family "monospace" :size 12)))
- (t
-  (setq doom-font (font-spec :family "monospace" :size 14)))
- )
