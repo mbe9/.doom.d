92f6dabfe10a6c64712fc7d1cb86dc8e45cc38b3
Pavel Pletnev <p.pletnev@yadro.com>
2023-02-23 13:01:39 +0300

    Enable aspell

--- init.el
+++ init.el
@@ -82,7 +82,7 @@
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       ;;spell             ; tasing you for misspelling mispelling
+       (spell +aspell +everywhere)             ; tasing you for misspelling mispelling
        ;;grammar           ; tasing grammar mistake every you make
 
        :tools
